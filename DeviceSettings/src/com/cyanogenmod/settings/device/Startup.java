7e9fced4a0e019acd4cd8fd740a0bbb1fc05ef2b
Daniel Hillenbrand <daniel.hillenbrand@codeworkx.de>
2012-08-12 11:32:30 +0200

    settings: restore ScreenFragmentActivity

--- DeviceSettings/src/com/cyanogenmod/settings/device/Startup.java
+++ DeviceSettings/src/com/cyanogenmod/settings/device/Startup.java
@@ -24,14 +24,23 @@ public class Startup extends BroadcastReceiver {
 
     @Override
     public void onReceive(final Context context, final Intent bootintent) {
+        DockFragmentActivity.restore(context);
+        HapticFragmentActivity.restore(context);
         mDNIeScenario.restore(context);
         mDNIeMode.restore(context);
         mDNIeNegative.restore(context);
         mDNIeOutdoor.restore(context);
+<<<<<<< HEAD
         HapticFragmentActivity.restore(context);
         VibratorIntensity.restore(context);
         SensorsFragmentActivity.restore(context);
         TouchkeyTimeout.restore(context);
+=======
+        RadioFragmentActivity.restore(context);
+        ScreenFragmentActivity.restore(context);
+        SensorsFragmentActivity.restore(context);
+        TouchkeyTimeout.restore(context);
+        VibratorIntensity.restore(context);
+>>>>>>> ba346b4... settings: restore ScreenFragmentActivity
     }
-
 }
